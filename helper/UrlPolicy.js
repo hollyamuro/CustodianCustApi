2510af06157a2b59c4f326fd9a264b5763c8f8b3
009727 <hollyamuro.lin@sinopac.com>
2018-08-19 13:47:26 +0800

    20180919最新版本

---
+++ helper/UrlPolicy.js
@@ -0,0 +1,16 @@
+/**
+ * 不須認證的URL列表
+ * @module helper/UrlPolicy.js
+ */
+
+"use strict";
+
+module.exports = [
+	"/",
+	"/login",
+	"/logout",
+	"/timeout",
+	"/error",
+	"/demo"
+	/*Add path which do not need auth*/
+];
